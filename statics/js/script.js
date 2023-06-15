bc118eeda82cea0072ea14b1545753cf5dc75976
PedroDB25 <pedro-0025@hotmail.com>
2023-06-15 23:14:39 +0200

    cambios 15 06

--- statics/js/script.js
+++ statics/js/script.js
@@ -2,6 +2,7 @@ window.addEventListener("load", init);
 
 function init() {
     [...document.querySelectorAll(".cartas a")].forEach(el=>el.addEventListener("click", verProyectos))
+    solicitarDatosRepo()
 }
 
 function verProyectos() {
@@ -12,4 +13,18 @@ function verProyectos() {
     [...document.querySelectorAll("#infoProyectos article")].forEach(el=>el.style.display="none");
     document.querySelectorAll("#infoProyectos article")[this.dataset.num-1].style.display="flex"
 
+}
+async function solicitarDatosRepo(){
+    let data = await fetch("https://api.github.com/users/PedroDB25")
+    let usuario = await data.json()
+    console.log(usuario)
+
+
+    let data2 = await fetch("https://api.github.com/users/PedroDB25/repos")
+    let usuario2 = await data2.json()
+    console.log(usuario2)
+
+    
+    document.querySelector("#usuario").innerHTML = usuario.login
+    document.querySelector("#descripcion").innerHTML = usuario.bio
 }
