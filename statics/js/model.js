0630bca313209e77701e22b1ce47e0793b9f8249
PedroDB25 <pedro-0025@hotmail.com>
2023-06-26 14:37:30 +0200

    inicio modulos

---
+++ statics/js/model.js
@@ -0,0 +1,37 @@
+export default class Proyecto{
+    constructor(proyecto){
+        this.proyecto=proyecto
+    }
+    getNodo(){
+        let caja = document.createElement("div")
+        let nombre = document.createElement("p")
+        let datos = document.createElement("div")
+        let version = document.createElement("div")
+        let verInfo = document.createElement("div")
+        let probar = document.createElement("a")
+        let verCodigo = document.createElement("a")
+
+        version.className = "version"
+        verInfo.className = "verInfo"
+        probar.className = "probar"
+        verCodigo.className = "verCodigo"
+
+        nombre.innerHTML= i.name
+        version.innerHTML= i.description
+        verInfo.innerHTML= "ver info"
+        probar.innerHTML= "probar"
+        verCodigo.innerHTML= "ver código"
+
+        verCodigo.href= i.html_url
+        verCodigo.target = "_blank"
+
+        datos.appendChild(version)
+        datos.appendChild(verInfo)
+        datos.appendChild(probar)
+        datos.appendChild(verCodigo)
+        caja.appendChild(nombre)
+        caja.appendChild(datos)
+
+        return caja
+    }
+}
--- statics/js/script.js
+++ statics/js/script.js
@@ -1,3 +1,5 @@
+import Proyecto from "./model";
+
 window.addEventListener("load", init);
 
 function init() {
@@ -22,62 +24,27 @@ async function solicitarDatosRepo() {
     document.querySelector("#usuario").innerHTML = usuario.login
     document.querySelector("#descripcion").innerHTML = usuario.bio
     document.querySelector("#fechaInicio").innerHTML = `Creación: ${usuario.created_at.split("T")[0].split("-").reverse().join("-")}`;
-    document.querySelector("#fechaActualizacion").innerHTML = `Última actualización: ${usuario.updated_at.split("T")[0].split("-").reverse().join("-")}`;
-
+    document.querySelector("#fechaActualizacion").innerHTML = `Actualización: ${usuario.updated_at.split("T")[0].split("-").reverse().join("-")}`;
 
     let data2 = await fetch("https://api.github.com/users/PedroDB25/repos")
     let proyectos = await data2.json()
     for (const i of proyectos) {
+        let proy = Proyecto(i);
 
-        let caja = document.createElement("div")
-        let nombre = document.createElement("p")
-        let datos = document.createElement("div")
-        let version = document.createElement("div")
-        let verInfo = document.createElement("div")
-        let probar = document.createElement("a")
-        let verCodigo = document.createElement("a")
-
-        version.className = "version"
-        verInfo.className = "verInfo"
-        probar.className = "probar"
-        verCodigo.className = "verCodigo"
-
-        nombre.innerHTML= i.name
-        version.innerHTML= i.description
-        verInfo.innerHTML= "ver info"
-        probar.innerHTML= "probar"
-        verCodigo.innerHTML= "ver código"
-
-        verCodigo.href= i.html_url
-        verCodigo.target = "_blank"
-
-        datos.appendChild(version)
-        datos.appendChild(verInfo)
-        datos.appendChild(probar)
-        datos.appendChild(verCodigo)
-        caja.appendChild(nombre)
-        caja.appendChild(datos)
-
-        console.log(i)
         switch (i.topics[0]) {
             case "reactjs":
-                document.querySelector("#infoProyectosReact").appendChild(caja)
+                document.querySelector("#infoProyectosReact").appendChild(proy.getNodo())
                 break;
             case "fastapi":
-                document.querySelector("#infoProyectosFastApi").appendChild(caja)
+                document.querySelector("#infoProyectosFastApi").appendChild(proy.getNodo())
                 break;
             case "spring":
-                document.querySelector("#infoProyectosSpring").appendChild(caja)
+                document.querySelector("#infoProyectosSpring").appendChild(proy.getNodo())
                 break;
 
             default:
-                document.querySelector("#infoProyectosVarios").appendChild(caja)
+                document.querySelector("#infoProyectosVarios").appendChild(proy.getNodo())
                 break;
         }
     }
-
-
-
-
-
 }
