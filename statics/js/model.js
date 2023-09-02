26249cdbc7803030af73bce10164dd0d82d96f41
PedroDB25 <pedro-0025@hotmail.com>
2023-09-02 23:53:28 +0200

    change-add: se utilizan .json para guardar la inforamacion.

--- statics/js/model.js
+++ statics/js/model.js
@@ -1,8 +1,12 @@
-export default class Proyecto{
-    constructor(proyecto){
-        this.proyecto=proyecto
+import proyectos from "/PedroDb25/statics/json/proyectos.json" assert { type: "json" };
+import proyectosNoSubidos from "/PedroDb25/statics/json/proyectosNoSubidos.json" assert { type: "json" };
+import tecnologias from "/PedroDb25/statics/json/tecnologias.json" assert { type: "json" };
+
+export default class Proyecto {
+    constructor(proyecto) {
+        this.proyecto = proyecto
     }
-    getNodo(){
+    getNodo() {
         let caja = document.createElement("div")
         let nombre = document.createElement("p")
         let datos = document.createElement("div")
@@ -16,13 +20,13 @@ export default class Proyecto{
         probar.className = "probar"
         verCodigo.className = "verCodigo"
 
-        nombre.innerHTML= this.proyecto.name
-        version.innerHTML= this.proyecto.description
-        verInfo.innerHTML= "ver info"
-        probar.innerHTML= "probar"
-        verCodigo.innerHTML= "ver código"
+        nombre.innerHTML = this.proyecto.name
+        version.innerHTML = this.proyecto.description
+        verInfo.innerHTML = "ver info"
+        probar.innerHTML = "probar"
+        verCodigo.innerHTML = "ver código"
 
-        verCodigo.href= this.proyecto.html_url
+        verCodigo.href = this.proyecto.html_url
         verCodigo.target = "_blank"
 
         datos.appendChild(version)
@@ -35,3 +39,131 @@ export default class Proyecto{
         return caja
     }
 }
+export function crearCartaGrupoProyectos() {
+    let marcocartas = document.querySelector(".cartas");
+    let contador = 1
+    for (const key in proyectos) {
+        let proyecto = proyectos[key];
+        //carta
+        let card = document.createElement("div")
+        card.classList.add("card");
+        card.style.width = "18rem";
+
+        //img
+        let img = document.createElement("img")
+        img.src = proyecto["url"];
+        img.alt = proyecto["alt"];
+        img.title = proyecto["title"]
+        img.className = "card-img-top img-fluid"
+
+        //cuerpo carta
+        let cardBody = document.createElement("div")
+        cardBody.classList.add("card-body")
+
+        let tituloCarta = document.createElement("h5")
+        tituloCarta.innerHTML = proyecto["titulo"]
+
+        let parrafoCarta = document.createElement("p")
+        parrafoCarta.className = "card-text"
+        parrafoCarta.classList.add(proyecto["classParrafo"])
+
+
+        let linkCarta = document.createElement("a")
+        linkCarta.dataset.num = contador++;
+        linkCarta.className = "btn btn-primary btncartaProyectos"
+        linkCarta.classList.add(proyecto["classLink"])
+
+        cardBody.appendChild(tituloCarta)
+        cardBody.appendChild(parrafoCarta)
+        cardBody.appendChild(linkCarta)
+
+        card.appendChild(img)
+        card.appendChild(cardBody)
+
+        marcocartas.appendChild(card)
+    }
+
+}
+
+export function crearBurbujasProyectos() {
+    for (const key in proyectosNoSubidos) {
+        let bloqueAInsertar = document.querySelector(`#${key}`)
+        let listaProyectos = proyectosNoSubidos[key]
+        for (const proyecto of proyectosNoSubidos[key]) {
+            let caja = document.createElement("div")
+            let titulo = document.createElement("p")
+            let datos = document.createElement("div")
+            let version = document.createElement("div")
+            let verInfo = document.createElement("div")
+
+            titulo.innerHTML = proyecto
+            version.innerHTML = "v0.0.1"
+            verInfo.innerHTML = "Ver info"
+            version.className = "version"
+            verInfo.className = "verInfo"
+
+            datos.appendChild(version)
+            datos.appendChild(verInfo)
+            caja.appendChild(titulo)
+            caja.appendChild(datos)
+
+            bloqueAInsertar.appendChild(caja)
+        }
+    }
+}
+
+export function crearBurbujasTecnologias() {
+    for (const key in tecnologias) {
+        let tecnologia = tecnologias[key]
+        let bloqueAInsertarTexto = document.querySelector(`article.${key}`)
+        let bloqueAInsertarBurbuja = document.querySelector(`article.${key} div.circulos`)
+
+        let titulo = document.createElement("h2")
+        titulo.innerHTML = tecnologia["titulo"]
+        bloqueAInsertarTexto.prepend(titulo)
+
+        for (const burbuja of tecnologia["burbujas"]) {
+            let bloque = document.createElement("div")
+            bloque.className = "circulo";
+            let img = document.createElement("img")
+            img.src = burbuja["src"];
+            img.alt = burbuja["alt"]
+
+            let texto = document.createElement("p")
+            texto.innerHTML = burbuja["titulo"]
+            
+            bloque.appendChild(img)
+            bloque.appendChild(texto)
+            bloqueAInsertarBurbuja.appendChild(bloque)
+        }
+
+
+
+
+
+
+
+        /*let listaProyectos = proyectosNoSubidos[key]
+        for (const proyecto of proyectosNoSubidos[key]) {
+            let caja = document.createElement("div")
+            let titulo = document.createElement("p")
+            let datos = document.createElement("div")
+            let version = document.createElement("div")
+            let verInfo = document.createElement("div")
+
+            titulo.innerHTML = proyecto
+            version.innerHTML = "v0.0.1"
+            verInfo.innerHTML = "Ver info"
+            version.className = "version"
+            verInfo.className = "verInfo"
+
+            datos.appendChild(version)
+            datos.appendChild(verInfo)
+            caja.appendChild(titulo)
+            caja.appendChild(datos)
+
+            bloqueAInsertar.appendChild(caja)
+        }*/
+    }
+}
+
--- statics/js/script.js
+++ statics/js/script.js
@@ -1,10 +1,18 @@
-import Proyecto from "/PedroDb25/statics/js/model.js";
+import Proyecto, { crearCartaGrupoProyectos, crearBurbujasProyectos, crearBurbujasTecnologias } from "/PedroDb25/statics/js/model.js";
+import mensajes from "/PedroDb25/statics/json/mensajes.json" assert { type: "json" };
 
 window.addEventListener("load", init);
 
-function init() {
-    [...document.querySelectorAll(".cartas a")].forEach(el => el.addEventListener("click", verProyectos))
-    solicitarDatosRepo()
+/**Se lanza al estar cargado el documento de JS
+ * 
+ */
+async function init() {
+    solicitarProyectos();
+    solicitarDatosRepo();
+    solicitarMensajes("es");
+    crearBurbujasProyectos();
+    crearBurbujasTecnologias();
+    [...document.querySelectorAll(".cartas a")].forEach(el => el.addEventListener("click", verProyectos));
 }
 
 function verProyectos() {
@@ -16,6 +24,9 @@ function verProyectos() {
     document.querySelectorAll("#infoProyectos article")[this.dataset.num - 1].style.display = "flex"
 
 }
+/**Solicita la informacion del respositorio
+ * 
+ */
 async function solicitarDatosRepo() {
 
     let data = await fetch("https://api.github.com/users/PedroDB25")
@@ -41,10 +52,18 @@ async function solicitarDatosRepo() {
             case "spring":
                 document.querySelector("#infoProyectosSpring").appendChild(proy.getNodo())
                 break;
-
             default:
                 document.querySelector("#infoProyectosVarios").appendChild(proy.getNodo())
                 break;
         }
     }
+}
+
+async function solicitarMensajes(lang) {
+    for (const clase in mensajes[lang]) {
+        document.querySelectorAll(`.${clase}`).forEach(el => el.innerHTML = mensajes[lang][clase])
+    }
+}
+async function solicitarProyectos() {
+    crearCartaGrupoProyectos()
 }
